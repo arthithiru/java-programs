efa9de29e3c6383b2f1ffb3eb052974da178e9e4
arthithiru <43032751+arthithiru@users.noreply.github.com>
2018-09-07 11:00:19 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int a=3,b=3,c;
+    c=a+b;
+    System.out.println("add the two numbers:"+c);
+    c=a-b;
+    System.out.println("subtract the two numbers:"+c);
+    c=a*b;
+    System.out.println("multiply the two numbers:"+c);
+    c=a/b;
+    System.out.println("divide the two numbers:"+c);
+  }
+}
