84b1570a3f9d5e91c6bbbf63fe065f010c16ada3
arthithiru <43032751+arthithiru@users.noreply.github.com>
2018-09-17 14:34:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a = 4 ;
+    int b = 6 ;
+    int c;
+    c = a+b;
+    System.out.println("a+b:"+c);
+    c = a-b;
+    System.out.println("a-b:"+c);
+    c = a*b;
+    System.out.println("a*b:"+c);
+    c = b/a;
+    System.out.println("b/a:"+c);
+    c = b%a;
+    System.out.println("b%a:"+c);
+    c = b++;
+    System.out.println("b++:"+c);
+    c = b--;
+    System.out.println("b--:"+c);
+    c = a&b;
+    System.out.println("a&b:"+c);
+    c = a|b;
+    System.out.println("a|b:"+c);
+    c = a^b;
+    System.out.println("a^b:"+c);
+    c = ~a;
+    System.out.println("~a:"+c);
+    c = a<<2;
+    System.out.println("a<<2:"+c);
+    c = a>>2;
+    System.out.println("a>>2:"+c);
+    c = a>>>2;
+    System.out.println("a>>>2:"+c);
+  }
+}
