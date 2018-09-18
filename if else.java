a3da446b3d6910c1ae75cc8324567ab8594f2480
arthithiru <43032751+arthithiru@users.noreply.github.com>
2018-09-18 14:53:37 +0530

    Add files via upload

---
+++ if else.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int DaysInTheatre = 50;
+  if (DaysInTheatre > 50)
+  System.out.println("DaysInTheatre is more than 50");
+  else
+  System.out.println("Flop"); 	
+	}
+}
